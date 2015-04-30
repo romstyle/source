12791d280da819dea4080b92df3a7e8fe978ac95
romstyle <rigaut.romain.77@gmail.com>
2015-04-30 23:51:10 +0200

    envoie

---
+++ Trinitycore/src/server/scripts/Outland/Auchindoun/SethekkHalls/instance_sethekk_halls.cpp
@@ -0,0 +1,143 @@
+/*
+ * Copyright (C) 2008-2013 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2006-2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* ScriptData
+SDName: Instance - Sethekk Halls
+SD%Complete: 50
+SDComment: Instance Data for Sethekk Halls instance
+SDCategory: Auchindoun, Sethekk Halls
+EndScriptData */
+
+#include "ScriptMgr.h"
+#include "InstanceScript.h"
+#include "sethekk_halls.h"
+
+class instance_sethekk_halls : public InstanceMapScript
+{
+public:
+    instance_sethekk_halls() : InstanceMapScript("instance_sethekk_halls", 556) { }
+
+    InstanceScript* GetInstanceScript(InstanceMap* map) const OVERRIDE
+    {
+        return new instance_sethekk_halls_InstanceMapScript(map);
+    }
+
+    struct instance_sethekk_halls_InstanceMapScript : public InstanceScript
+    {
+        instance_sethekk_halls_InstanceMapScript(Map* map) : InstanceScript(map)
+        {
+            SetBossNumber(EncounterCount);
+        }
+
+        void Initialize() OVERRIDE
+        {
+            SetBossState(DATA_ANZU, NOT_STARTED);
+            iIkissDoorGUID = 0;
+        }
+
+        void OnCreatureCreate(Creature* creature) OVERRIDE
+        {
+            if (creature->GetEntry() == NPC_ANZU)
+            {
+                if (GetBossState(DATA_ANZU) == DONE)
+                    creature->DisappearAndDie();
+                else
+                    SetBossState(DATA_ANZU, IN_PROGRESS);
+            }
+        }
+
+        void OnGameObjectCreate(GameObject* go) OVERRIDE
+        {
+             if (go->GetEntry() == GO_IKISS_DOOR)
+                iIkissDoorGUID = go->GetGUID();
+        }
+
+        bool SetBossState(uint32 type, EncounterState state) OVERRIDE
+        {
+            if (!InstanceScript::SetBossState(type, state))
+                return false;
+
+            switch (type)
+            {
+                case DATA_DARKWEAVER_SYTH:
+                    break;
+                case DATA_TALON_KING_IKISS:
+                    if (state == DONE)
+                        DoUseDoorOrButton(iIkissDoorGUID, DAY*IN_MILLISECONDS);
+                    break;
+                case DATA_ANZU:
+                    break;
+                default:
+                    break;
+            }
+
+            return true;
+        }
+
+        std::string GetSaveData() OVERRIDE
+        {
+            OUT_SAVE_INST_DATA;
+
+            std::ostringstream saveStream;
+            saveStream << "S H " << GetBossSaveData();
+
+            OUT_SAVE_INST_DATA_COMPLETE;
+            return saveStream.str();
+        }
+
+        void Load(const char* str) OVERRIDE
+        {
+            if (!str)
+            {
+                OUT_LOAD_INST_DATA_FAIL;
+                return;
+            }
+
+            OUT_LOAD_INST_DATA(str);
+
+            char dataHead1, dataHead2;
+
+            std::istringstream loadStream(str);
+            loadStream >> dataHead1 >> dataHead2;
+
+            if (dataHead1 == 'S' && dataHead2 == 'H')
+            {
+                for (uint32 i = 0; i < EncounterCount; ++i)
+                {
+                    uint32 tmpState;
+                    loadStream >> tmpState;
+                    if (tmpState == IN_PROGRESS || tmpState > SPECIAL)
+                        tmpState = NOT_STARTED;
+                    SetBossState(i, EncounterState(tmpState));
+                }
+            }
+            else
+                OUT_LOAD_INST_DATA_FAIL;
+
+            OUT_LOAD_INST_DATA_COMPLETE;
+        }
+
+        protected:
+            uint64 iIkissDoorGUID;
+    };
+};
+
+void AddSC_instance_sethekk_halls()
+{
+    new instance_sethekk_halls();
+}
