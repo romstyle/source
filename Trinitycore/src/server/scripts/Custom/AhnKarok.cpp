8585660d2f65ce4b33547818829f392c3158f6a1
Oxym <atidote63240@gmail.com>
2015-05-06 01:23:45 +0200

    BOSS 1
    Ajout des sons

--- Trinitycore/src/server/scripts/Custom/AhnKarok.cpp
+++ Trinitycore/src/server/scripts/Custom/AhnKarok.cpp
@@ -11,7 +11,12 @@ enum Spells
 };
 enum Sounds
 {
-	SANG_ET_CHAIR = 20000,
+	AHRKAROK_CARNAGE = 20000,
+	AHRKAROK_PAS_ECHAPATOIR = 20001,
+	AHRKAROK_COULER_LE_SANG = 20002,
+	AHRKAROK_EVENT_1 = 20003,
+	AHRKAROK_DEAD = 20004,
+	AHRKAROK_KILL = 20005,
 };
 class boss_ahnkarok : public CreatureScript
 {
@@ -55,11 +60,13 @@ public:
 		void KilledUnit(Unit* /*victim*/) OVERRIDE
         {
 			me->MonsterYell("Délicieux bain de sang !", LANG_UNIVERSAL, me->GetGUID());
+			Talk(AHRKAROK_KILL);
         }
 		void JustDied(Unit* killer) OVERRIDE
         {
 			_JustDied();
-			me->MonsterYell("Imp-imposs-Aaaaaaaaaaarrrrh...", LANG_UNIVERSAL, me->GetGUID());
+			me->MonsterYell("Aaaaaaaaaaarrrrh... Mrphhh... Mrphhh... !!", LANG_UNIVERSAL, me->GetGUID());
+			Talk(AHRKAROK_DEAD);
         }
 		void UpdateAI(uint32 diff) OVERRIDE
         {
@@ -68,35 +75,38 @@ public:
             if (enchainement <= diff)
             {
 				DoCast(SelectTarget(SELECT_TARGET_TOPAGGRO, 0, 100, true), SPELL_ENCHAINEMENT);
-                enchainement = 60000;
+                enchainement = 15000;
 				me->MonsterYell("Je vais faire un feu avec vos restes !", LANG_UNIVERSAL, me->GetGUID());
             }
 			else enchainement -= diff;
             if (fendre_armure <= diff)
             {
 				DoCast(SelectTarget(SELECT_TARGET_TOPAGGRO, 0, 100, true), SPELL_FENDRE_ARMURE);
-                fendre_armure = urand(10000, 20000);
-				me->MonsterYell("VIENS LA !", LANG_UNIVERSAL, me->GetGUID());
+				fendre_armure = 7000;
+				me->MonsterYell("Pas d'échappatoire !!", LANG_UNIVERSAL, me->GetGUID());
+				Talk(AHRKAROK_PAS_ECHAPATOIR);
             }
 			else fendre_armure -= diff;
             if (dechainement_de_lame <= diff)
             {
 				DoCast(SelectTarget(SELECT_TARGET_TOPAGGRO, 0, 100, true), SPELL_DECHAINEMENT_DE_LAME);
                 dechainement_de_lame = 45000;
-				me->MonsterYell("SANG ET CHAIR !!!", LANG_UNIVERSAL, me->GetGUID());
-				Talk(SANG_ET_CHAIR);
+				me->MonsterYell("CARNAGE !!", LANG_UNIVERSAL, me->GetGUID());
+				Talk(AHRKAROK_CARNAGE);
             }
 			else dechainement_de_lame -= diff;
 			if (agitation <= diff)
             {
 				DoCast(SelectTarget(SELECT_TARGET_RANDOM, 0, 100, true), SPELL_AGITTION_FRENETIQUE);
-                agitation = 75000;
-				me->MonsterYell("Rhaaaaaaa MOURREZ !!", LANG_UNIVERSAL, me->GetGUID());
+                agitation = 35000;
+				me->MonsterYell("Il est temps de faire couler le SANG !!!", LANG_UNIVERSAL, me->GetGUID());
+				Talk(AHRKAROK_COULER_LE_SANG);
             }
 			else agitation -= diff;
 			if (me->HealthBelowPct(10) && buffed == false)
 			{
-				me->MonsterYell("JE VAIS BRISER VOS OS !", LANG_UNIVERSAL, me->GetGUID());
+				me->MonsterYell("Mahhhhh... J'ai joué avec vous assez longtemps !!", LANG_UNIVERSAL, me->GetGUID());
+				Talk(AHRKAROK_EVENT_1);
 				DoCast(me, SPELL_BERSERK, true);
 				buffed = true;
 			}
